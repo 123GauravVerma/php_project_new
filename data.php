6de8157b2c1645ce7531de3974a7d60de3b49691
123GauravVerma <103870349+123GauravVerma@users.noreply.github.com>
2022-12-22 13:42:59 +0000

    First commit for the day!

--- data.php
+++ data.php
@@ -117,3 +117,4 @@
 249 days ago <?php echo "Welcome to new project!";?>
 248 days ago <?php echo "Welcome to new project!";?>
 247 days ago <?php echo "Welcome to new project!";?>
+246 days ago <?php echo "Welcome to new project!";?>
